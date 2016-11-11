f99d619a43ca9c6db6d7cb7c29a76618c3449617
Alec Linder (RIT Student) <afl4947@ad.rit.edu>
2016-11-10 21:29:16 -0500

    The laser machine looks nice, added some asssets so we can have some things flying around.

---
+++ Assets/Scripts/ActivatedLightIndicator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ShipComponent))]
+public class ActivatedLightIndicator : MonoBehaviour {
+
+    public MeshRenderer meshRender;
+    public Material activatedMat;
+    public Material deactivatedMat;
+
+    private ShipComponent shipComponent;
+
+	// Use this for initialization
+	void Start () {
+        shipComponent = GetComponent<ShipComponent>();
+        shipComponent.OnActivated += Activated;
+        shipComponent.OnDeactivate += Deactivated;
+        Deactivated();
+    }
+
+    public void Activated()
+    {
+        meshRender.material = activatedMat;
+    }
+
+    public void Deactivated()
+    {
+        meshRender.material = deactivatedMat;
+    }
+}
---
+++ Assets/Scripts/ShipComponent.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public delegate void ShippActivated();
+public delegate void ShipDeactivated();
+
+public class ShipComponent : MonoBehaviour {
+
+    private event ShippActivated onActivated;
+    private event ShipDeactivated onDeactivated;
+
+    private bool isActivated = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void Activate()
+    {
+        if(!isActivated)
+        {
+            isActivated = true;
+            if (onActivated != null)
+            {
+                onActivated.Invoke();
+            }
+        }
+
+    }
+
+    public void Deactivate()
+    {
+        if (isActivated)
+        {
+            isActivated = false;
+            if (onDeactivated != null)
+            {
+                onDeactivated.Invoke();
+            }
+        }
+    }
+
+    public ShippActivated OnActivated
+    {
+        get { return onActivated; }
+        set { onActivated = value; }
+    }
+
+    public ShipDeactivated OnDeactivate
+    {
+        get { return onDeactivated; }
+        set { onDeactivated = value; }
+    }
+}
